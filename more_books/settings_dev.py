a905966f2b6ed5db20ef52f2920e8ed4624127c7
ka113private <ka113private@gmail.com>
2023-01-12 07:34:36 +0900

    [add]問い合わせメール送信機能の実装

---
+++ more_books/settings_dev.py
@@ -0,0 +1,52 @@
+from .settings_common import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-0d@uc9k=_4=wqjk*wf4z=&1o(3&alfq8wu41^9u1xdg1$7pft+'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# ロギング設定
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+
+    #ロガーの設定
+    'loggers': {
+        #Djangoが利用するロガー
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        },
+        #booksアプリケーションが利用するロガー
+        'books': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    },
+
+    #ハンドラの設定
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'dev'
+        },
+    },
+
+    #フォーマッタの設定
+    'formatters': {
+        'dev':{
+            'format': '\t'.join([
+                '%(asctime)s',
+                '[%(levelname)s]',
+                '%(pathname)s(Line:%(lineno)d)',
+                '%(message)s'
+            ])
+        }
+    }
+}
