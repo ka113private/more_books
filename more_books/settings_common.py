0ac37edfd53acbbd32c1dd6c92b6af78af00401e
ka113private <ka113private@gmail.com>
2023-01-15 22:39:04 +0900

    [add]Django-allauthを用いた認証機能の追加

--- more_books/settings_common.py
+++ more_books/settings_common.py
@@ -36,8 +36,46 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'books.apps.BooksConfig',
-    'accounts.apps.AccountsConfig'
+    'accounts.apps.AccountsConfig',
+    
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'django_bootstrap5',
+
 ]
+
+# django-allauthの設定
+# django-allauthで利用するdjango.contrib.sitesを使うためにサイト識別用IDを設定
+SITE_ID = 1
+
+AUTHENTICATION_BACKENDS = (
+    'allauth.account.auth_backends.AuthenticationBackend',#一般ユーザー用(メールアドレス認証)
+    'django.contrib.auth.backends.ModelBackend',#管理サイト用（ユーザー名認証）
+)
+
+# メールアドレス認証に変更する設定
+ACCOUNT_AUTHENTICATION_METHOD = 'email'
+ACCOUNT_USERNAME_REQUIRED = False
+
+# サインアップにメールアドレス確認をはさむように設定
+ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+ACCOUNT_EMAIL_REQUIRED = True
+
+# ログイン/ログアウト後の遷移先を設定
+LOGIN_REDIRECT_URL = 'books:index'
+ACCOUNT_LOGOUT_REDIRECT_URL = 'account_login'
+
+# ログアウトリンクのクリック一発でログアウトする設定
+ACCOUNT_LOGOUT_ON_GET = True
+
+# Django-allauthが送信するメールの件名に自動付与される接頭辞をブランクにする設定
+ACCOUNT_EMAIL_SUBJECT_PREFIX = ''
+
+# デフォルトのメールの送信元設定
+DEFAULT_FROM_EMAIL = os.environ.get('FROM_EMAIL')
+
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
